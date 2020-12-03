10cc681a3ab83eaaf23be3915660a9e4e9d583f1
Branc0 <rrochadeazevedo@gmail.com>
2020-12-03 10:41:56 -0300

    Changing App name, from "WeHoo" to "Mais Facil"

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
     it('should display welcome message', () => {
         page.navigateTo();
-        expect(page.getTitleText()).toEqual('agencia3r-wehoo-frontend-admin app is running!');
+        expect(page.getTitleText()).toEqual('agencia3r-maisfacil-frontend-admin app is running!');
     });
 
     afterEach(async () => {
--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
             clearContext: false // leave Jasmine Spec Runner output visible in browser
         },
         coverageIstanbulReporter: {
-            dir: require('path').join(__dirname, './coverage/agencia3r-wehoo-frontend-admin'),
+            dir: require('path').join(__dirname, './coverage/agencia3r-maisfacil-frontend-admin'),
             reports: ['html', 'lcovonly', 'text-summary'],
             fixWebpackSourcePaths: true
         },

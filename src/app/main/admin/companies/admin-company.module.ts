9e4917e0dc7f97cd50a713a5f5f6280a77448bc0
Branc0 <rrochadeazevedo@gmail.com>
2020-12-09 12:48:23 -0300

    fixing missing import of  company list module

--- src/app/main/admin/companies/admin-company.module.ts
+++ src/app/main/admin/companies/admin-company.module.ts
@@ -1,12 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { AdminCompanyRoutingModule } from './admin-company-routing.module';
+import { CompanyListModule } from './company-list/company-list.module';
 
 @NgModule({
     declarations: [],
     imports: [
         CommonModule,
         AdminCompanyRoutingModule,
+        CompanyListModule
     ],
     exports: [],
 })

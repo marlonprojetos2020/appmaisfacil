7b6fd2f3e939345d6c52d8cabba15efe38f1abf1
Leonardo <Petinari>
2020-12-17 16:52:29 -0300

    admin dashboard

--- src/app/main/admin/admin-dashboard/admin-dashboard/admin-dashboard.component.ts
+++ src/app/main/admin/admin-dashboard/admin-dashboard/admin-dashboard.component.ts
@@ -3,14 +3,15 @@ import { PoPageAction } from '@po-ui/ng-components';
 
 @Component({
     templateUrl: './admin-dashboard.component.html',
+    styleUrls: ['./admin-dashboard.component.scss'],
 })
 export class AdminDashboardComponent implements OnInit {
 
     actions: Array<PoPageAction> = [
         {
-            label: 'Nova Empresa',
-            icon: 'po-icon-plus-circle',
-            url: 'admin/empresas/nova-empresa',
+            label: 'Novo Usuario',
+            icon: 'po-icon po-icon-user-add',
+            url: '/admin',
         },
     ];
 

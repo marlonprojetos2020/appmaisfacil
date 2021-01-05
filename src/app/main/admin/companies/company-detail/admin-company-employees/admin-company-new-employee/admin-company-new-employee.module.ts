e50a2bb34052d15b532cc23b728547296314f4cf
Leonardo <Petinari>
2021-01-05 15:14:10 -0300

    admin company new employee done

--- src/app/main/admin/companies/company-detail/admin-company-employees/admin-company-new-employee/admin-company-new-employee.module.ts
+++ src/app/main/admin/companies/company-detail/admin-company-employees/admin-company-new-employee/admin-company-new-employee.module.ts
@@ -1,11 +1,11 @@
 import {NgModule} from '@angular/core';
 import {AdminCompanyNewEmployeeComponent} from './admin-company-new-employee/admin-company-new-employee.component';
 import {CommonModule} from '@angular/common';
-import {PoContainerModule, PoFieldModule, PoPageModule} from '@po-ui/ng-components';
+import {PoButtonModule, PoContainerModule, PoFieldModule, PoPageModule} from '@po-ui/ng-components';
 
 @NgModule({
     declarations: [AdminCompanyNewEmployeeComponent],
-    imports: [CommonModule, PoPageModule, PoContainerModule, PoFieldModule],
+    imports: [CommonModule, PoPageModule, PoContainerModule, PoFieldModule, PoButtonModule],
 })
 export class AdminCompanyNewEmployeeModule {
 }

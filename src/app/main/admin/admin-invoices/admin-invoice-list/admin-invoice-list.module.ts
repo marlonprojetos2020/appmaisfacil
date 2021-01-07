e2a8cb08ac202300292cebea2d30aaa456e2ad25
Leonardo <Petinari>
2021-01-07 15:23:10 -0300

    admin invoice first page

--- src/app/main/admin/admin-invoices/admin-invoice-list/admin-invoice-list.module.ts
+++ src/app/main/admin/admin-invoices/admin-invoice-list/admin-invoice-list.module.ts
@@ -1,10 +1,10 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {PoPageModule} from '@po-ui/ng-components';
-import {AdminInvoiceListComponent} from './admin-invoice-list/admin-invoice-list.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AdminInvoiceListComponent } from './admin-invoice-list/admin-invoice-list.component';
+import { PageDatatableModule } from '../../../../shared/components/page-datatable/page-datatable.module';
 
 @NgModule({
-    imports: [CommonModule, PoPageModule],
+    imports: [CommonModule, PageDatatableModule],
     declarations: [AdminInvoiceListComponent],
     exports: [AdminInvoiceListComponent],
 })
--- src/app/main/admin/admin-invoices/admin-invoice-list/admin-invoice-list/admin-invoice-list.component.ts
+++ src/app/main/admin/admin-invoices/admin-invoice-list/admin-invoice-list/admin-invoice-list.component.ts
@@ -1,6 +1,34 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
+import { PoPageAction, PoTableAction } from '@po-ui/ng-components';
+import { environment } from '../../../../../../environments/environment';
+import { DatatableColumn } from '../../../../../shared/components/page-datatable/page-datatable/datatable-column';
 
 @Component({
     templateUrl: './admin-invoices-list.component.html',
 })
-export class AdminInvoiceListComponent {}
+export class AdminInvoiceListComponent {
+    pageActions: PoPageAction[] = [];
+
+    serviceApi = `${environment.apiUrl}/users/p/search`;
+    tableActions: PoTableAction[] = [];
+    columns: DatatableColumn[] = [
+        {
+            label: 'Situação',
+            property: 'userCompany.fantasyName',
+        },
+        {
+            label: 'Empresa',
+            property: 'userCompany.cnpj',
+        },
+        {
+            label: 'Valor',
+            property: 'userCompany.cnpj',
+        },
+        {
+            label: 'Emissão',
+            property: 'name',
+        },
+    ];
+
+    constructor() {}
+}

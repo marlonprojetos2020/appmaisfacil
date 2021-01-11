a3dc028a9ef95ad87550163a81ff24116fe8798f
Leonardo <Petinari>
2021-01-11 11:19:25 -0300

    admin company new associate

--- src/app/main/admin/companies/company-detail/admin-company-associates/admin-company-new-associate/admin-company-new-associate.module.ts
+++ src/app/main/admin/companies/company-detail/admin-company-associates/admin-company-new-associate/admin-company-new-associate.module.ts
@@ -1,9 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminCompanyNewAssociateComponent } from './admin-company-new-associate/admin-company-new-associate.component';
+import {
+    PoButtonModule,
+    PoContainerModule,
+    PoFieldModule,
+    PoPageModule,
+} from '@po-ui/ng-components';
 
 @NgModule({
-    imports: [CommonModule],
+    imports: [
+        CommonModule,
+        PoPageModule,
+        PoContainerModule,
+        PoFieldModule,
+        PoButtonModule,
+    ],
     declarations: [AdminCompanyNewAssociateComponent],
 })
 export class AdminCompanyNewAssociateModule {}
--- src/app/main/admin/companies/company-detail/admin-company-associates/admin-company-new-associate/admin-company-new-associate/admin-company-new-associate.component.ts
+++ src/app/main/admin/companies/company-detail/admin-company-associates/admin-company-new-associate/admin-company-new-associate/admin-company-new-associate.component.ts
@@ -2,5 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
     templateUrl: 'admin-company-new-associate.component.html',
+    styleUrls: ['admin-company-new-associate.component.scss'],
 })
 export class AdminCompanyNewAssociateComponent {}

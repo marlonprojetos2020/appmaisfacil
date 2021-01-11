5121506d5c0f5b25759e4718c8e4324849cbb3ac
Leonardo <Petinari>
2021-01-11 11:47:25 -0300

    change layout buttons

--- src/app/main/admin/companies/company-detail/admin-company-extra-services/admin-company-extra-service.module.ts
+++ src/app/main/admin/companies/company-detail/admin-company-extra-services/admin-company-extra-service.module.ts
@@ -1,11 +1,21 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {PoContainerModule, PoPageModule} from '@po-ui/ng-components';
-import {AdminCompanyExtraServiceComponent} from './admin-company-extra-service/admin-company-extra-service.component';
-import {CardExtraServiceModule} from '../../../../../shared/components/card-extra-service/card-extra-service.module';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+    PoButtonModule,
+    PoContainerModule,
+    PoPageModule,
+} from '@po-ui/ng-components';
+import { AdminCompanyExtraServiceComponent } from './admin-company-extra-service/admin-company-extra-service.component';
+import { CardExtraServiceModule } from '../../../../../shared/components/card-extra-service/card-extra-service.module';
 
 @NgModule({
-    imports: [CommonModule, PoPageModule, PoContainerModule, CardExtraServiceModule],
+    imports: [
+        CommonModule,
+        PoPageModule,
+        PoContainerModule,
+        CardExtraServiceModule,
+        PoButtonModule,
+    ],
     declarations: [AdminCompanyExtraServiceComponent],
 })
 export class AdminCompanyExtraServiceModule {}

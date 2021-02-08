dc53af3d286763a27d3d7acdac679f63d1e197e9
Leonardo <Petinari>
2021-02-08 14:47:32 -0300

    acordions company dashboard

--- src/app/main/admin/companies/company-detail/dashboard/dashboard/company-detail-dashboard.component.ts
+++ src/app/main/admin/companies/company-detail/dashboard/dashboard/company-detail-dashboard.component.ts
@@ -4,6 +4,7 @@ import { PoBreadcrumb, PoPageAction } from '@po-ui/ng-components';
 import { Observable } from 'rxjs';
 import { CompaniesService } from '../../../companies.service';
 import { User } from '../../../model/user';
+import { CompanyDashboardService } from '../../../../../company/company-dashboard/company-dashboard.service';
 
 @Component({
     templateUrl: './company-detail-dashboard.component.html',
@@ -17,6 +18,60 @@ export class CompanyDetailDashboardComponent implements OnInit {
         items: [],
     };
 
+    billingPending = [];
+
+    billingRefused = [];
+
+    billingPendingReview = [];
+
+    statementsPending = [];
+
+    statementsPendingReview = [];
+
+    employeePendingFired = [];
+
+    notaFiscalCancelada = [];
+
+    notaFiscalProcessando = [];
+
+    items: [];
+
+    // TEXTO DOS ACCORDIONS
+
+    billingPendingText = '';
+
+    billingRefusedText = '';
+
+    billingPendingReviewText = '';
+
+    statementsPendingText = '';
+
+    statementsPendingReviewText = '';
+
+    employeePendingFiredText = '';
+
+    notaFiscalCanceladaText = '';
+
+    notaFiscalProcessandoText = '';
+
+    // Verifica se esta tudo ok pra alterar o estilo do accordion
+
+    billingPendingOk = false;
+
+    billingRefusedOk = false;
+
+    billingPendingReviewOk = false;
+
+    statementsPendingOk = false;
+
+    statementsPendingReviewOk = false;
+
+    employeePendingFiredOk = false;
+
+    notaFiscalCanceladaOk = false;
+
+    notaFiscalProcessandoOk = false;
+
     public readonly actions: Array<PoPageAction> = [
         {
             label: 'Editar Empresa',
@@ -26,7 +81,8 @@ export class CompanyDetailDashboardComponent implements OnInit {
 
     constructor(
         private activetedRoute: ActivatedRoute,
-        private companyDetailService: CompaniesService
+        private companyDetailService: CompaniesService,
+        private companyDashboardService: CompanyDashboardService
     ) {}
 
     ngOnInit(): void {
@@ -37,6 +93,155 @@ export class CompanyDetailDashboardComponent implements OnInit {
         this.companyDetailService
             .getUserCompany(this.activetedRoute.snapshot.params.id)
             .subscribe((data) => this.setBreadcrumb(data));
+
+        this.companyDashboardService
+            .getBillingStatus('PENDING')
+            .subscribe((data) => {
+                if (data.items.length === 0) {
+                    this.billingPendingText = 'Não há cobrancas pendentes';
+                } else {
+                    data.items.map((billing) => {
+                        this.billingPending.push(billing.description);
+                        this.billingPendingText =
+                            'Você tem cobranças pendentes :';
+                        this.billingPendingOk = true;
+                    });
+                }
+            });
+
+        this.companyDashboardService
+            .getBillingStatus('REFUSED')
+            .subscribe((data) => {
+                if (data.items.length === 0) {
+                    this.billingRefusedText = 'Não há cobranças recusadas';
+                } else {
+                    data.items.map((billing) => {
+                        this.billingRefused.push(billing.description);
+                        this.billingRefusedText =
+                            'Você tem cobranças recusadas :';
+                        this.billingRefusedOk = true;
+                    });
+                }
+            });
+
+        this.companyDashboardService
+            .getBillingStatus('PENDING_REVIEW')
+            .subscribe((data) => {
+                if (data.items.length === 0) {
+                    this.billingPendingReviewText =
+                        'Não há cobranças em analise';
+                } else {
+                    data.items.map((billing) => {
+                        this.billingPendingReview.push(billing.description);
+                        this.billingPendingReviewOk = true;
+                        this.billingPendingReviewText =
+                            'Você tem cobranças em analise :';
+                    });
+                }
+            });
+
+        // Get Statements
+
+        this.companyDashboardService
+            .getStatementsStatus('PENDING')
+            .subscribe((data) => {
+                if (data.items.length === 0) {
+                    this.statementsPendingText = 'Não há extratos pendentes';
+                } else {
+                    data.items.map((statements) => {
+                        this.statementsPending.push(
+                            'Banco:' +
+                                ' ' +
+                                statements.bankAccount.bankName +
+                                ' - ' +
+                                'Mês Referente:' +
+                                ' ' +
+                                statements.month
+                        );
+                        this.statementsPendingOk = true;
+                        this.statementsPendingText =
+                            'Você tem extratos pendentes :';
+                    });
+                }
+            });
+
+        this.companyDashboardService
+            .getStatementsStatus('PENDING_REVIEW')
+            .subscribe((data) => {
+                if (data.items.length === 0) {
+                    this.statementsPendingReviewText =
+                        'Não há extratos em analise';
+                } else {
+                    data.items.map((statements) => {
+                        this.statementsPendingReview.push(
+                            'Banco:' +
+                                ' ' +
+                                statements.bankAccount.bankName +
+                                ' - ' +
+                                'Mês Referente:' +
+                                ' ' +
+                                statements.month
+                        );
+                        this.statementsPendingReviewOk = true;
+                        this.statementsPendingReviewText =
+                            'Você tem extratos em analise :';
+                    });
+                }
+            });
+
+        this.companyDashboardService
+            .getEmployessStatus('PENDING_FIRED')
+            .subscribe((data) => {
+                if (data.items.length === 0) {
+                    this.employeePendingFiredText =
+                        'Não há demissões pendentes';
+                } else {
+                    data.items.map((employee) => {
+                        this.employeePendingFired.push(employee.name);
+                        this.employeePendingFiredOk = true;
+                        this.employeePendingFiredText =
+                            'Você tem demissões pendentes :';
+                    });
+                }
+            });
+
+        this.companyDashboardService
+            .getNotaFiscalStatus('PROCESSING')
+            .subscribe((data) => {
+                if (data.items.length === 0) {
+                    this.notaFiscalProcessandoText =
+                        'Não há notas ficais em analise';
+                } else {
+                    data.items.map((nota) => {
+                        this.notaFiscalProcessando.push(
+                            nota.client.name +
+                                ' ' +
+                                'total' +
+                                ' ' +
+                                nota.totalAmount
+                        );
+                        this.notaFiscalProcessandoText =
+                            'Você tem notas fiscais em analise :';
+                        this.notaFiscalProcessandoOk = true;
+                    });
+                }
+            });
+
+        this.companyDashboardService
+            .getNotaFiscalStatus('WAITING_CANCELEMENT')
+            .subscribe((data) => {
+                if (data.items.length === 0) {
+                    this.notaFiscalCanceladaText =
+                        'Não há notas ficais em analise';
+                } else {
+                    data.items.map((nota) => {
+                        this.notaFiscalCancelada.push(nota.client.name);
+                        this.notaFiscalCanceladaOk = true;
+                        this.notaFiscalCanceladaText =
+                            'Você tem notas fiscais canceladas :';
+                    });
+                }
+            });
     }
 
     setBreadcrumb(user: User): void {
--- src/app/main/company/company-dashboard/company-dashboard.component.ts
+++ src/app/main/company/company-dashboard/company-dashboard.component.ts
@@ -27,11 +27,41 @@ export class CompanyDashboardComponent implements OnInit {
 
     items: [];
 
+    // TEXTO DOS ACCORDIONS
+
+    billingPendingText = '';
+
+    billingRefusedText = '';
+
+    billingPendingReviewText = '';
+
+    statementsPendingText = '';
+
+    statementsPendingReviewText = '';
+
+    employeePendingFiredText = '';
+
+    notaFiscalCanceladaText = '';
+
+    notaFiscalProcessandoText = '';
+
     // Verifica se esta tudo ok pra alterar o estilo do accordion
 
-    billingPendingReviewOk: boolean;
+    billingPendingOk = false;
 
-    billingPendingOk: boolean;
+    billingRefusedOk = false;
+
+    billingPendingReviewOk = false;
+
+    statementsPendingOk = false;
+
+    statementsPendingReviewOk = false;
+
+    employeePendingFiredOk = false;
+
+    notaFiscalCanceladaOk = false;
+
+    notaFiscalProcessandoOk = false;
 
     constructor(private companyDashboardService: CompanyDashboardService) {}
 
@@ -40,90 +70,151 @@ export class CompanyDashboardComponent implements OnInit {
 
         this.companyDashboardService
             .getBillingStatus('PENDING')
-            .subscribe((data) =>
-                data.items.map((billing) => {
-                    this.billingPending.push(billing.description);
-                })
-            );
+            .subscribe((data) => {
+                if (data.items.length === 0) {
+                    this.billingPendingText = 'Não há cobrancas pendentes';
+                } else {
+                    data.items.map((billing) => {
+                        this.billingPending.push(billing.description);
+                        this.billingPendingText =
+                            'Você tem cobranças pendentes :';
+                        this.billingPendingOk = true;
+                    });
+                }
+            });
 
         this.companyDashboardService
             .getBillingStatus('REFUSED')
-            .subscribe((data) =>
-                data.items.map((billing) =>
-                    this.billingRefused.push(billing.description)
-                )
-            );
+            .subscribe((data) => {
+                if (data.items.length === 0) {
+                    this.billingRefusedText = 'Não há cobranças recusadas';
+                } else {
+                    data.items.map((billing) => {
+                        this.billingRefused.push(billing.description);
+                        this.billingRefusedText =
+                            'Você tem cobranças recusadas :';
+                        this.billingRefusedOk = true;
+                    });
+                }
+            });
 
         this.companyDashboardService
             .getBillingStatus('PENDING_REVIEW')
             .subscribe((data) => {
-                data.items.map((billing) =>
-                    this.billingPendingReview.push(billing.description)
-                );
+                if (data.items.length === 0) {
+                    this.billingPendingReviewText =
+                        'Não há cobranças em analise';
+                } else {
+                    data.items.map((billing) => {
+                        this.billingPendingReview.push(billing.description);
+                        this.billingPendingReviewOk = true;
+                        this.billingPendingReviewText =
+                            'Você tem cobranças em analise :';
+                    });
+                }
             });
 
         // Get Statements
 
         this.companyDashboardService
             .getStatementsStatus('PENDING')
-            .subscribe((data) =>
-                data.items.map((statements) =>
-                    this.statementsPending.push(
-                        'Banco:' +
-                            ' ' +
-                            statements.bankAccount.bankName +
-                            ' - ' +
-                            'Mês Referente:' +
-                            ' ' +
-                            statements.month
-                    )
-                )
-            );
+            .subscribe((data) => {
+                if (data.items.length === 0) {
+                    this.statementsPendingText = 'Não há extratos pendentes';
+                } else {
+                    data.items.map((statements) => {
+                        this.statementsPending.push(
+                            'Banco:' +
+                                ' ' +
+                                statements.bankAccount.bankName +
+                                ' - ' +
+                                'Mês Referente:' +
+                                ' ' +
+                                statements.month
+                        );
+                        this.statementsPendingOk = true;
+                        this.statementsPendingText =
+                            'Você tem extratos pendentes :';
+                    });
+                }
+            });
 
         this.companyDashboardService
             .getStatementsStatus('PENDING_REVIEW')
-            .subscribe((data) =>
-                data.items.map((statements) =>
-                    this.statementsPendingReview.push(
-                        'Banco:' +
-                            ' ' +
-                            statements.bankAccount.bankName +
-                            ' - ' +
-                            'Mês Referente:' +
-                            ' ' +
-                            statements.month
-                    )
-                )
-            );
+            .subscribe((data) => {
+                if (data.items.length === 0) {
+                    this.statementsPendingReviewText =
+                        'Não há extratos em analise';
+                } else {
+                    data.items.map((statements) => {
+                        this.statementsPendingReview.push(
+                            'Banco:' +
+                                ' ' +
+                                statements.bankAccount.bankName +
+                                ' - ' +
+                                'Mês Referente:' +
+                                ' ' +
+                                statements.month
+                        );
+                        this.statementsPendingReviewOk = true;
+                        this.statementsPendingReviewText =
+                            'Você tem extratos em analise :';
+                    });
+                }
+            });
 
         this.companyDashboardService
             .getEmployessStatus('PENDING_FIRED')
-            .subscribe((data) =>
-                data.items.map((employee) =>
-                    this.employeePendingFired.push(employee.name)
-                )
-            );
+            .subscribe((data) => {
+                if (data.items.length === 0) {
+                    this.employeePendingFiredText =
+                        'Não há demissões pendentes';
+                } else {
+                    data.items.map((employee) => {
+                        this.employeePendingFired.push(employee.name);
+                        this.employeePendingFiredOk = true;
+                        this.employeePendingFiredText =
+                            'Você tem demissões pendentes :';
+                    });
+                }
+            });
 
         this.companyDashboardService
             .getNotaFiscalStatus('PROCESSING')
-            .subscribe((data) =>
-                data.items.map((nota) =>
-                    this.notaFiscalProcessando.push(
-                        nota.client.name +
-                            ' ' +
-                            'total' +
-                            ' ' +
-                            nota.totalAmount
-                    )
-                )
-            );
+            .subscribe((data) => {
+                if (data.items.length === 0) {
+                    this.notaFiscalProcessandoText =
+                        'Não há notas ficais em analise';
+                } else {
+                    data.items.map((nota) => {
+                        this.notaFiscalProcessando.push(
+                            nota.client.name +
+                                ' ' +
+                                'total' +
+                                ' ' +
+                                nota.totalAmount
+                        );
+                        this.notaFiscalProcessandoText =
+                            'Você tem notas fiscais em analise :';
+                        this.notaFiscalProcessandoOk = true;
+                    });
+                }
+            });
 
         this.companyDashboardService
             .getNotaFiscalStatus('WAITING_CANCELEMENT')
-            .subscribe((data) =>
-                data.items.map((nota) =>
-                    this.notaFiscalCancelada.push(nota.client.name)
-                )
-            );
+            .subscribe((data) => {
+                if (data.items.length === 0) {
+                    this.notaFiscalCanceladaText =
+                        'Não há notas ficais em analise';
+                } else {
+                    data.items.map((nota) => {
+                        this.notaFiscalCancelada.push(nota.client.name);
+                        this.notaFiscalCanceladaOk = true;
+                        this.notaFiscalCanceladaText =
+                            'Você tem notas fiscais canceladas :';
+                    });
+                }
+            });
     }
 }

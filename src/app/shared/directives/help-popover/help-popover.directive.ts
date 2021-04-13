48dc3a905ff224d3946eaa2a128874009337e1ba
Branc0 <rrochadeazevedo@gmail.com>
2021-04-13 20:39:12 -0300

    fixing directive to show page functionality

--- src/app/shared/directives/help-popover/help-popover.directive.ts
+++ src/app/shared/directives/help-popover/help-popover.directive.ts
@@ -1,12 +1,12 @@
 import {
     AfterViewInit,
-    Directive,
     ElementRef,
     Input,
     Renderer2,
     ViewChild,
+    Directive,
 } from '@angular/core';
-import { PoModalComponent } from '@po-ui/ng-components';
+import { PoDialogAlertOptions, PoDialogService, PoModalComponent } from '@po-ui/ng-components';
 
 @Directive({
     selector: '[appHelpPopOver]',
@@ -15,22 +15,28 @@ export class HelpPopoverDirective implements AfterViewInit {
 
     @Input() helpText: string;
 
+    title = '';
+
     pageHeaderElement: any;
 
     constructor(
         private el: ElementRef,
         private renderer: Renderer2,
+        private poDialogService: PoDialogService,
     ) {}
 
     ngAfterViewInit(): void {
         this.pageHeaderElement = this.el.nativeElement
-            .querySelector('.po-page-header');
-
-        this.pageHeaderElement
-            .addEventListener('click', this.showPopover.bind(this));
+            .querySelector('.po-page-header-title');
+        this.title = this.pageHeaderElement.textContent;
+        this.pageHeaderElement.addEventListener('click', this.showPopover.bind(this));
     }
 
     private showPopover(): void {
-        alert(this.helpText);
+        const alertOptions: PoDialogAlertOptions = {
+            title: this.title,
+            message: this.helpText,
+        };
+        this.poDialogService.alert(alertOptions);
     }
 }
